dd71419c96ef8e6771e34d213a97fdddd1e17390
joashneves <joashneves@gmail.com>
2025-05-27 18:49:24 -0300

    coloca barra

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import 'primeicons/primeicons.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -8,11 +8,10 @@ import router from './router'
 
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
+import Button from "primevue/button"
 
 const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
+//app.component('Button', Button);
 app.use(PrimeVue, {
   // Default theme configuration
   theme: {
@@ -24,5 +23,8 @@ app.use(PrimeVue, {
     },
   },
 })
+
+app.use(createPinia())
+app.use(router)
 
 app.mount('#app')
