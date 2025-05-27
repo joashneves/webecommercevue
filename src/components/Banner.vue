11c64b1253d2de789900860f6a2c05143b437e5e
Joashneves <141419406+joashneves@users.noreply.github.com>
2025-05-27 18:49:33 +0000

    coloca primevue e dark mode

---
+++ src/components/Banner.vue
@@ -0,0 +1,9 @@
+<script setup>
+function toggleDarkMode() {
+  document.documentElement.classList.toggle('my-app-dark')
+}
+</script>
+
+<template>
+  <Button label="Toggle Dark Mode" @click="toggleDarkMode()" />
+</template>
