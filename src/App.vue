6cd2df11fca19bf825c3974c727ca73ad0bb019d
joashneves <joashneves@gmail.com>
2025-05-28 21:22:53 -0300

    card de produtos

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Banner from './components/Banner.vue'
+import Card from './components/Card.vue';
+const dadomokado = [ {
+  titulo: "dadomokado",
+  descricao: "descricao top",
+  valor: 19
+} ,
+{
+  titulo: "titulo menos top",
+  descricao: "descricao menos top",
+  valor: 129
+} ,
+{
+  titulo: "titulo 4",
+  descricao: "descricao mais top",
+  valor: 129
+} ]
 </script>
 
 <template>
   <Banner />
+  <Card titulo="titulo 1" descricao="Descrição" valor="99"/>
+  
+  <Card 
+  v-for="cards in dadomokado"
+  :titulo="cards.titulo" :descricao="cards.descricao" :valor="cards.valor"/>
 </template>
 
 <style scoped>
---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Card from 'primevue/card';
+defineProps({
+  titulo : String,
+  descricao: String,
+  valor: Number
+})
+</script>
+
+<template>
+  
+<Card>
+    <template #title>{{ titulo }}</template>
+    <template #content>
+        <p class="m-0">
+            {{ descricao}}
+        </p>
+        <p> {{ valor }}</p>
+    </template>
+</Card>
+  
+</template>
