eb7628ede50ebfce9b1bdc0c3fd352d66b13c880
joashneves <joashneves@gmail.com>
2025-05-29 00:36:50 -0300

    home

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,28 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Banner from './components/Banner.vue'
-import Card from './components/Card.vue';
-const dadomokado = [ {
-  titulo: "dadomokado",
-  descricao: "descricao top",
-  valor: 19
-} ,
-{
-  titulo: "titulo menos top",
-  descricao: "descricao menos top",
-  valor: 129
-} ,
-{
-  titulo: "titulo 4",
-  descricao: "descricao mais top",
-  valor: 129
-} ]
+import Card from './components/Card.vue'
+const dadomokado = [ { titulo: 'dadomokado', descricao: 'descricao top', valor: 19, },
+  { titulo: 'titulo menos top', descricao: 'descricao menos top', valor: 129, },
+  { titulo: 'titulo 4', descricao: 'descricao mais top', valor: 129, }, 
+{ titulo: 'titulo 12  ', descricao: 'descricao menos ou mais top', valor: 912, },
+{ titulo: 'titulo 5', descricao: 'descricao mais amsod top', valor: 1229, }]
+const conteudo = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(4, 1fr)', // 4 colunas de largura igual
+  gap: '1rem', // espaço entre os cards
+  justifyItems: 'center', // centraliza os itens dentro das células
+}
 </script>
 
 <template>
   <Banner />
-  <Card titulo="titulo 1" descricao="Descrição" valor="99"/>
-  
-  <Card 
-  v-for="cards in dadomokado"
-  :titulo="cards.titulo" :descricao="cards.descricao" :valor="cards.valor"/>
+  <div :style="conteudo">
+    <Card
+      v-for="cards in dadomokado"
+      :titulo="cards.titulo"
+      :descricao="cards.descricao"
+      :valor="cards.valor"
+    />
+  </div>
 </template>
-
-<style scoped>
-
-</style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { Button } from 'primevue';
 import Card from 'primevue/card';
 defineProps({
   titulo : String,
@@ -9,13 +10,22 @@ defineProps({
 
 <template>
   
-<Card>
+<Card  role="region" style="width: 25rem; overflow: hidden">
+        <template #header>
+        <img alt="user header" src="https://i.ytimg.com/vi/h6uRI_MJwdI/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AHUBoAC4AOKAgwIABABGGUgRihDMA8=&rs=AOn4CLCWM2hmbF85D-PvnplmVVoWXO-hDw" />
+    </template>
     <template #title>{{ titulo }}</template>
     <template #content>
         <p class="m-0">
             {{ descricao}}
         </p>
         <p> {{ valor }}</p>
+    </template>
+        <template #footer>
+        <div class="flex gap-4 mt-1">
+            <Button label="Inspecionar" severity="secondary" outlined class="w-full" />
+            <Button label="Comprar" class="w-full" />
+        </div>
     </template>
 </Card>
   
